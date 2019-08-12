e1c078fc4aa378e5911c2f7572df8efe0fd55ef4
Eugene Yong <eugeneyjy@yahoo.com>
2019-08-12 23:29:47 +0800

    Fixed merge problem

--- app.cpp
+++ app.cpp
@@ -8,13 +8,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
   char c;
-<<<<<<< HEAD
   int n_customer = 0, choice = 0 , result_count = 0, r_num = 0;
-=======
-  int n_customer = 0, choice = 0;
->>>>>>> 1288442a12d354981c845deb954c179efebde020
   struct customer* customers;
-  struct customer* result;
+  struct customer* result;//The result u pass in to the advance search
   ifstream infile;
   ofstream outfile;
   valid_cla(argc, argv);//validation of command line argument
@@ -35,15 +31,13 @@ int main(int argc, char *argv[])
   {
     choice = 0;
     ask_choice(choice);
-<<<<<<< HEAD
     run_option(&customers, &result, n_customer, result_count, r_num, choice);
   } while(choice != 6);
   save_data(customers, n_customer, r_num, argv, outfile);
-=======
-    run_option(&customers, n_customer, choice);
-  } while(choice != 5);
-  save_data(customers, n_customer, argv, outfile);
->>>>>>> 1288442a12d354981c845deb954c179efebde020
   free_customer(&customers);
+  if(result_count > 0)
+  {
+    free_customer(&result);
+  }
   return 0;
 }
